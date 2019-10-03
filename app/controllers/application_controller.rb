e3948747dc49bd7abe131788538ccff20ef752a6
github username <github email address>
2019-10-03 15:54:27 +0100

    Admin team view page & sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
 
     def current_user
         if session[:user_id]
-        User.find(session[:user_id])
+            User.find(session[:user_id])
+            elsif session[:admin_id]
+            Admin.find(session[:admin_id])
         end
     end 
 
@@ -14,11 +16,25 @@ class ApplicationController < ActionController::Base
           end
     end
 
-    def authorize_gold_user
-        if current_user.standard_user?
-            flash[:notice] = 'Access only for Gold Members'
-            redirect_to teams_path
+    def authorize_admin 
+        unless current_user.class == Admin
+            flash[:notice] = 'Access only for Admins'
+                redirect_to teams_path
         end
+    end
+
+    def authorize_gold_user
+        # method can't be called on admin! || current_user.class = Admin.class
+        unless current_user.class == Admin
+            if current_user.standard_user? 
+                flash[:notice] = 'Access only for Gold Members'
+                redirect_to teams_path
+            end
+        end
+    end
+
+    def authorize_admin_and_gold_user
+        # authorize both
     end
 
 end
